e5b1bdf2223b0af17953c7e20269eb906571b4f5
LucianaAlmeda <lucianaoalmeda@gmail.com>
2024-05-14 08:53:11 -0300

    Faz extracao de funcao

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -24,11 +24,11 @@ struct ContentView: View {
             
             Text("Anos").font(.body1)
             TextField ("Quantos anos completos tem seu cão", value: $years, format: .number)
-
+            
             
             Text("Meses").font(.body1)
             TextField ("E quantos meses ele tem", value: $months, format: .number)
-
+            
             
             Text("Porte").font(.body1)
             
@@ -55,12 +55,12 @@ struct ContentView: View {
             Spacer()
             Button("Cãocular", action: processYears).font(.body1)
             
-            .foregroundStyle(.white)
-            .bold()
-            .frame(maxWidth: .infinity)
-            .frame(height: 50)
-            .background(.indigo)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
+                .foregroundStyle(.white)
+                .bold()
+                .frame(maxWidth: .infinity)
+                .frame(height: 50)
+                .background(.indigo)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
         }
         .textFieldStyle(.roundedBorder)
         .keyboardType(.numberPad) //para nao aparecer letras
@@ -78,35 +78,10 @@ struct ContentView: View {
             print("pelo menos um campo deve ser maior que zero")
             return }
         
-        //o resultado vai ser os anos * multiplicador + a fração do ano em meses * multiplicador
-        //multiplicador:
-        //* pequeno:6
-        //* medio:7
-        //* grande:8
+        result = porteSelected.calcularIdade(deAnos: years, eMeses: months)
         
-        let multiplicador: Int
-        switch porteSelected {
-        case .pequeno :
-            multiplicador = 6
-        case .médio:
-            multiplicador = 7
-        case .grande:
-            multiplicador=8
+        #Preview {
+            ContentView()
         }
-        
-        //o resultado vai ser anos * 7
-        //result = years * 7
-        //o resultado vau ser os anos * 7 + a fração do ano em meses * 7
-        //result = years * 7 + months * 7/12
-        // multiplicador depende e varia de acordo com o porte 
-        
-        
-        result = years * multiplicador + months * multiplicador/12
-        
     }
-
-}
-
-#Preview {
-    ContentView()
 }
--- Caoculadora/Caoculadora/porte .swift
+++ Caoculadora/Caoculadora/porte .swift
@@ -11,4 +11,34 @@ enum Porte: String, CaseIterable {
     case pequeno = "Pequeno"
     case médio = "Médio"
     case grande = "Grande"
+    
+    func calcularIdade(deAnos anosCaninos: Int, eMeses mesesCaninos: Int) -> Int {
+        
+        //o resultado vai ser os anos * multiplicador + a fração do ano em meses * multiplicador
+        //multiplicador:
+        //* pequeno:6
+        //* medio:7
+        //* grande:8
+        
+        let multiplicador: Int
+        switch self {
+        case .pequeno :
+            multiplicador = 6
+        case .médio:
+            multiplicador = 7
+        case .grande:
+            multiplicador=8
+        }
+        
+        //o resultado vai ser anos * 7
+        //result = years * 7
+        //o resultado vau ser os anos * 7 + a fração do ano em meses * 7
+        //result = years * 7 + months * 7/12
+        // multiplicador depende e varia de acordo com o porte
+        
+        
+        let result = anosCaninos * multiplicador + mesesCaninos * multiplicador/12
+        return result
+    }
 }
+
